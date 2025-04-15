ade36181e4df2fdd8c536537c6bc5d4b56bc5b6a
ornaras <ornaras.us@gmail.com>
2025-04-15 10:23:51 +0400

    Добавлена библиотека с структурами чека и информацией о генерации отчёта

---
+++ Enums/SessionStatus.cs
@@ -0,0 +1,21 @@
+﻿namespace RetailCorrector
+{
+    /// <summary>
+    /// Статус смены
+    /// </summary>
+    public enum SessionStatus 
+    { 
+        /// <summary>
+        /// Смена закрыта
+        /// </summary>
+        Closed, 
+        /// <summary>
+        /// Смена открыта
+        /// </summary>
+        Opened, 
+        /// <summary>
+        /// Смена просрочена (открыта более 24 часов)
+        /// </summary>
+        Expired 
+    }
+}
---
+++ Receipt.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RetailCorrector
+{
+    /// <summary>
+    /// Информация о чеке
+    /// </summary>
+    public struct Receipt
+    {
+        /// <summary>
+        /// Дата и время отбития оригинального чека
+        /// </summary>
+        public DateTime Created { get; set; }
+        /// <summary>
+        /// Тип чека
+        /// </summary>
+        public Operation Operation { get; set; }
+        /// <summary>
+        /// Позиции
+        /// </summary>
+        public Position[] Items { get; set; }
+        /// <summary>
+        /// Фискальный признак
+        /// </summary>
+        public string FiscalSign { get; set; }
+        /// <summary>
+        /// Тип коррекции
+        /// </summary>
+        public CorrType CorrectionType { get; set; }
+        /// <summary>
+        /// Номер документа (акта), поясняющего причину создания чека(ов) коррекции<br/>
+        /// </summary>
+        public string ActNumber { get; set; }
+        /// <summary>
+        /// Оплата
+        /// </summary>
+        public Payment Payment { get; set; }
+        /// <summary>
+        /// Округлённая итоговая сумма
+        /// </summary>
+        public uint? RoundedSum { get; set; }
+    }
+}
