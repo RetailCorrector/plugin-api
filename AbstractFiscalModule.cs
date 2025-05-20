7ad4c40c7c2bfa7b6d3f0f35141cb91aac935163
ornaras <ornaras.us@gmail.com>
2025-05-20 11:04:30 +0400

    Переработка API

---
+++ AbstractFiscalModule.cs
@@ -0,0 +1,16 @@
+﻿namespace RetailCorrector
+{
+    public abstract class AbstractFiscalModule
+    {
+        public event Action<bool, string, Exception?>? OnLog;
+        public event Action<string>? OnNotify;
+
+        protected internal void Log(bool isError, string message, Exception? exception = null) =>
+            OnLog?.Invoke(isError, message, exception);
+        protected internal void Notify(string message) => OnNotify?.Invoke(message);
+
+        public abstract Task<bool> OnLoad();
+        public abstract Task<bool> ProcessingReceipt(Receipt receipt);
+        public abstract Task OnUnload();
+    }
+}
