e2c3b13840b64cff79a1d9dcd650846bbe20733a
ornaras <ornaras.us@gmail.com>
2025-05-30 15:43:57 +0400

    Добавление тип содержания

--- Report.cs
+++ Report.cs
@@ -8,5 +8,6 @@ namespace RetailCorrector
         public string Url { get; set; }
         public Dictionary<string, List<string>>? Headers { get; set; }
         public string Content { get; set; }
+        public string ContentType { get; set; }
     }
 }
