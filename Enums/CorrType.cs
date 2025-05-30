e1e666315450766c821704d5e2896396efba3032
ornaras <ornaras.us@gmail.com>
2025-05-30 12:58:34 +0400

    Добавлены отображаемые имена для элементов CorrType

--- Enums/CorrType.cs
+++ Enums/CorrType.cs
@@ -1,4 +1,6 @@
-﻿namespace RetailCorrector
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetailCorrector
 {
     /// <summary>
     /// Тип коррекции
@@ -8,10 +10,12 @@
         /// <summary>
         /// Самостоятельная коррекция
         /// </summary>
+        [Display(Name = "Самостоятельно")]
         ByYourself = 0,
         /// <summary>
         /// Коррекция по предписанию ФНС
         /// </summary>
+        [Display(Name = "По предписанию")]
         ByTaxService = 1
     }
 }
