8392ee5d6c00eedb4d1426262472475fc9900001
ornaras <ornaras.us@gmail.com>
2025-06-03 12:57:05 +0400

    Переработка Fiscal

--- AbstractFiscalModule.cs
+++ AbstractFiscalModule.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.Loader;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.Loader;
 
 namespace RetailCorrector
 {
@@ -6,13 +8,21 @@ namespace RetailCorrector
     {
         public event Action<bool, string, Exception?>? OnLog;
         public event Action<string>? OnNotify;
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public event Action<int>? Started;
+        public event Action<int>? ProgressUpdated;
 
         protected internal void Log(bool isError, string message, Exception? exception = null) =>
             OnLog?.Invoke(isError, message, exception);
         protected internal void Notify(string message) => OnNotify?.Invoke(message);
+        protected internal void OnStarted(int maxProgress) => Started?.Invoke(maxProgress);
+        protected internal void OnProgressUpdated(int value) => ProgressUpdated?.Invoke(value);
+        protected internal void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
         public abstract Task<uint> CountUnsendDocs();
-        public abstract Task<bool> OnLoad(AssemblyLoadContext assembly);
+        public abstract Task OnLoad(AssemblyLoadContext assembly);
+        public abstract Task<bool> Connect();
         public abstract Task<bool> ProcessingReceipt(Receipt receipt);
         public abstract Task OnUnload();
     }
