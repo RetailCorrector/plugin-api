1b0f438088ed5e08889b114dac9a803d9b739c82
ornaras <ornaras.us@gmail.com>
2025-06-19 10:16:22 +0400

    Добавлена документация для Report и PositionCode

--- PositionCode.cs
+++ PositionCode.cs
@@ -1,33 +1,77 @@
-﻿using System.Reflection.Emit;
-using System.Runtime.InteropServices.JavaScript;
-using System.Xml.Linq;
-
-namespace RetailCorrector
+﻿namespace RetailCorrector
 {
     /// <summary>
     /// Информация об коде товара
     /// </summary>
     public struct PositionCode()
     {
+        /// <summary>
+        /// Нераспознаный код
+        /// </summary>
         public string Unknown { get; set; } = "";
+        /// <summary>
+        /// EAN-8
+        /// </summary>
         public string EAN8 { get; set; } = "";
+        /// <summary>
+        /// EAN-13
+        /// </summary>
         public string EAN13 { get; set; } = "";
+        /// <summary>
+        /// ITF-14
+        /// </summary>
         public string ITF14 { get; set; } = "";
+        /// <summary>
+        /// GS1.0
+        /// </summary>
         public string GS1_0 { get; set; } = "";
+        /// <summary>
+        /// GS1.M
+        /// </summary>
         public string GS1_M { get; set; } = "";
+        /// <summary>
+        /// Компактный маркировочный код
+        /// </summary>
         public string KMK { get; set; } = "";
+        /// <summary>
+        /// Код мехового изделия
+        /// </summary>
         public string MI { get; set; } = "";
+        /// <summary>
+        /// ЕГАИС2.0
+        /// </summary>
         public string EGAIS2 { get; set; } = "";
+        /// <summary>
+        /// ЕГАИС3.0
+        /// </summary>
         public string EGAIS3 { get; set; } = "";
+        /// <summary>
+        /// Ф.1
+        /// </summary>
         public string F1 { get; set; } = "";
+        /// <summary>
+        /// Ф.2
+        /// </summary>
         public string F2 { get; set; } = "";
+        /// <summary>
+        /// Ф.3
+        /// </summary>
         public string F3 { get; set; } = "";
+        /// <summary>
+        /// Ф.4
+        /// </summary>
         public string F4 { get; set; } = "";
+        /// <summary>
+        /// Ф.5
+        /// </summary>
         public string F5 { get; set; } = "";
+        /// <summary>
+        /// Ф.6
+        /// </summary>
         public string F6 { get; set; } = "";
 
         /// <summary>
-        /// Клонирование 
+        /// Клонирование кодов позиции
         /// </summary>
         public PositionCode Clone() => new()
         {
--- Report.cs
+++ Report.cs
@@ -1,11 +1,29 @@
 ﻿namespace RetailCorrector
 {
+    /// <summary>
+    /// Структура отчёта
+    /// </summary>
     public struct Report
     {
+        /// <summary>
+        /// Метод запроса
+        /// </summary>
         public HttpMethod Method { get; set; }
+        /// <summary>
+        /// Адрес запроса
+        /// </summary>
         public string Url { get; set; }
+        /// <summary>
+        /// Заголовки запроса
+        /// </summary>
         public Dictionary<string, List<string>>? Headers { get; set; }
+        /// <summary>
+        /// Тело запроса
+        /// </summary>
         public string Content { get; set; }
+        /// <summary>
+        /// Заголовок "Content-Type" запроса
+        /// </summary>
         public string ContentType { get; set; }
     }
 }
