56173b1db0f0515beec1b24ede0915506a400fd0
ornaras <85742833+ornaras@users.noreply.github.com>
2025-06-25 11:35:14 +0400

    Настройка основных конструкторов (#1)
    * Receipt
    * Position
    * Payment

--- Payment.cs
+++ Payment.cs
@@ -3,28 +3,28 @@
     /// <summary>
     /// Информация об оплате чека
     /// </summary>
-    public struct Payment
+    public struct Payment()
     {
         /// <summary>
         /// Наличная оплата
         /// </summary>
-        public uint Cash { get; set; }
+        public uint Cash { get; set; } = 0;
         /// <summary>
         /// Безналичная оплата
         /// </summary>
-        public uint ECash { get; set; }
+        public uint ECash { get; set; } = 0;
         /// <summary>
         /// Предоплата (аванс)
         /// </summary>
-        public uint Pre { get; set; }
+        public uint Pre { get; set; } = 0;
         /// <summary>
         /// Постаоплата (в кредит)
         /// </summary>
-        public uint Post { get; set; }
+        public uint Post { get; set; } = 0;
         /// <summary>
         /// Встречное предоставление
         /// </summary>
-        public uint Provision { get; set; }
+        public uint Provision { get; set; } = 0;
 
         public static explicit operator uint[](Payment payment) =>
         [
