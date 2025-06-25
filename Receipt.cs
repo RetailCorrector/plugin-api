e0d8dec2aeff9d9dcb0a5bf0897d79a375ab0a34
ornaras <ornaras.us@gmail.com>
2025-06-25 11:33:20 +0400

    Настроен основной конструтктор Receipt

--- Receipt.cs
+++ Receipt.cs
@@ -3,48 +3,48 @@
     /// <summary>
     /// Информация о чеке
     /// </summary>
-    public struct Receipt
+    public struct Receipt()
     {
         /// <summary>
         /// Дата и время отбития оригинального чека
         /// </summary>
-        public DateTime Created { get; set; }
+        public DateTime Created { get; set; } = DateTime.Today;
         /// <summary>
         /// Тип чека
         /// </summary>
-        public Operation Operation { get; set; }
+        public Operation Operation { get; set; } = Operation.Income;
         /// <summary>
         /// Позиции
         /// </summary>
-        public Position[] Items { get; set; }
+        public Position[] Items { get; set; } = [];
         /// <summary>
         /// Фискальный признак
         /// </summary>
-        public string FiscalSign { get; set; }
+        public string FiscalSign { get; set; } = "";
         /// <summary>
         /// Тип коррекции
         /// </summary>
-        public CorrType CorrectionType { get; set; }
+        public CorrType CorrectionType { get; set; } = CorrType.ByYourself;
         /// <summary>
         /// Номер документа (акта), поясняющего причину создания чека(ов) коррекции<br/>
         /// </summary>
-        public string? ActNumber { get; set; }
+        public string? ActNumber { get; set; } = null;
         /// <summary>
         /// Оплата
         /// </summary>
-        public Payment Payment { get; set; }
+        public Payment Payment { get; set; } = new();
         /// <summary>
         /// Итоговая сумма
         /// </summary>
-        public uint TotalSum { get; set; }
+        public uint TotalSum { get; set; } = 0;
         /// <summary>
         /// Отраслевой реквизит чека
         /// </summary>
-        public IndustryData[] IndustryData { get; set; }
+        public IndustryData[] IndustryData { get; set; } = [];
         /// <summary>
         /// Дополнительные данные
         /// </summary>
-        public Dictionary<string, string> Additions { get; private set; }
+        public Dictionary<string, string> Additions { get; private set; } = [];
 
         /// <summary>
         /// Клонирование чека
