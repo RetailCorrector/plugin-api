bb49e97366b619bc693dc9e464b35993d58dcbd5
ornaras <ornaras.us@gmail.com>
2025-06-25 11:33:28 +0400

    Настроен основной конструтктор Position

--- Position.cs
+++ Position.cs
@@ -3,52 +3,52 @@
     /// <summary>
     /// Информация о позиции
     /// </summary>
-    public struct Position
+    public struct Position()
     {
         /// <summary>
         /// Наименование
         /// </summary>
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
         /// <summary>
         /// Тип оплаты
         /// </summary>
-        public PaymentType PayType { get; set; }
+        public PaymentType PayType { get; set; } = PaymentType.FullPayment;
         /// <summary>
         /// Тип позиции
         /// </summary>
-        public PositionType PosType { get; set; }
+        public PositionType PosType { get; set; } = PositionType.Product;
         /// <summary>
         /// Налоговая ставка
         /// </summary>
-        public TaxRate TaxRate { get; set; }
+        public TaxRate TaxRate { get; set; } = TaxRate.Tax10;
         /// <summary>
         /// Единица измерений
         /// </summary>
-        public MeasureUnit MeasureUnit { get; set; }
+        public MeasureUnit MeasureUnit { get; set; } = MeasureUnit.None;
         /// <summary>
         /// Цена
         /// </summary>
-        public uint Price { get; set; }
+        public uint Price { get; set; } = 0;
         /// <summary>
         /// Количество
         /// </summary>
-        public uint Quantity { get; set; }
+        public uint Quantity { get; set; } = 1;
         /// <summary>
         /// Итоговая сумма
         /// </summary>
-        public uint TotalSum { get; set; }
+        public uint TotalSum { get; set; } = 0;
         /// <summary>
         /// Отраслевой реквизит позиции
         /// </summary>
-        public IndustryData[] IndustryData { get; set; }
+        public IndustryData[] IndustryData { get; set; } = [];
         /// <summary>
         /// Коды товара
         /// </summary>
-        public PositionCode Codes { get; set; }
+        public PositionCode Codes { get; set; } = new();
         /// <summary>
         /// Дополнительные данные
         /// </summary>
-        public Dictionary<string, string> Additions { get; private set; }
+        public Dictionary<string, string> Additions { get; private set; } = [];
         
         /// <summary>
         /// Клонирование позиции
