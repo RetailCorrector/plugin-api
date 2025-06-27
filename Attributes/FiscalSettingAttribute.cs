6934d0ba5fa95d4a81bb67d869db9a05b3677287
ornaras <ornaras.us@gmail.com>
2025-06-27 11:21:58 +0400

    Добавлен атрибут регистрации параметров фискальной интеграции

---
+++ Attributes/FiscalSettingAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace RetailCorrector.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class FiscalSettingAttribute(string id, string displayName) : Attribute
+    {
+        public string Id { get; } = id;
+        public string DisplayName { get; } = displayName;
+    }
+}
